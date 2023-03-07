5d0544219e4da208f9702793b4a4c5306fd3dd0e
Andrew Owino <andystudy4@gmail.com>
2023-03-07 19:39:17 +0300

    Clean up

--- lib/file_explorer.rb
+++ lib/file_explorer.rb
@@ -22,34 +22,15 @@ class FileExplorer
 
     File.foreach(file_path) do |line|
       if line.start_with?('$ cd')
-        if line.split(' ')[-1] == '..'
-          # Go back
-          current_directory = current_directory.parent
-        elsif line.split(' ')[-1] != '/'
-          directory_name = line.split(' ')[-1]
-
-          current_directory = Directory.new(name: directory_name, parent: parent_directory)
-
-          current_directory = bfs(current_directory, directory_name)
-
-          parent_directory = current_directory
-        end
+        current_directory, parent_directory = change_directory(current_directory, line, parent_directory)
       else
         if line.start_with?('dir')
-          directory_name = line.split(' ')[-1]
-
-          new_directory = Directory.new(name: directory_name, parent: parent_directory)
-
-          parent_directory.child_directories << new_directory
-
-        elsif line.split(' ')[0] != '$' && Integer(line.split(' ')[0])
-          # It's a file
+          create_directory(line, parent_directory)
+        elsif is_file?(line)
           parent_directory.files << line
         end
 
-        size = FileExplorer.calculate_size(parent_directory)
-
-        directories[current_directory.name] = size if size > 0 && size <= max_size
+        check_directory_size(current_directory, directories, max_size, parent_directory)
       end
     end
 
@@ -79,5 +60,41 @@ class FileExplorer
 
   def self.total_size(directories: {})
     directories.values.sum
+  end
+
+  private
+
+  def self.is_file?(line)
+    line.split(' ')[0] != '$' && Integer(line.split(' ')[0])
+  end
+
+  def self.create_directory(line, parent_directory)
+    directory_name = line.split(' ')[-1]
+
+    new_directory = Directory.new(name: directory_name, parent: parent_directory)
+
+    parent_directory.child_directories << new_directory
+  end
+
+  def self.change_directory(current_directory, line, parent_directory)
+    if line.split(' ')[-1] == '..'
+      # Go back
+      current_directory = current_directory.parent
+    elsif line.split(' ')[-1] != '/'
+      directory_name = line.split(' ')[-1]
+
+      current_directory = Directory.new(name: directory_name, parent: parent_directory)
+
+      current_directory = bfs(current_directory, directory_name)
+
+      parent_directory = current_directory
+    end
+    return current_directory, parent_directory
+  end
+
+  def self.check_directory_size(current_directory, directories, max_size, parent_directory)
+    size = FileExplorer.calculate_size(parent_directory)
+
+    directories[current_directory.name] = size if size > 0 && size <= max_size
   end
 end
