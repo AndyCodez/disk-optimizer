a5cc59980b96f91048fa23a6c6f99f3e8688fe07
Andrew Owino <andystudy4@gmail.com>
2023-03-07 04:39:42 +0300

    add application prompts and example input file

---
+++ lib/disk-optimizer.rb
@@ -0,0 +1,23 @@
+require_relative './directory'
+require_relative './file_explorer'
+
+print 'Enter the maximum directory size in MB: '
+max_size = gets.chomp.to_i
+
+file_path = 'uploads/terminal_input.txt'
+
+puts 'Processing...'
+
+directories = FileExplorer.get_directories(file_path, max_size)
+
+puts 'Done!'
+
+puts "Here is a list of directories that are at most #{max_size} MB:"
+
+directories.each do |key, val|
+  puts "#{key}: #{val} MB"
+end
+
+total_size = FileExplorer.total_size(directories: directories)
+
+puts "Their total size is #{total_size} MB"
