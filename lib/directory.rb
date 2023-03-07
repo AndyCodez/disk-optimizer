aaff38442e882460c0717dc706acddc31c0e2f25
Andrew Owino <andystudy4@gmail.com>
2023-03-07 04:01:21 +0300

    omit unused :size attribute on directory

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,5 +1,5 @@
 class Directory
-  attr_accessor :name, :files, :size, :child_directories, :visited
+  attr_accessor :name, :files, :child_directories, :visited
   attr_reader :parent
 
   def initialize(name:, parent: '')
